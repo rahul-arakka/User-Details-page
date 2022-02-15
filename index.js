5c1c0a7a0a8c9d820e6ba2db7ab4528536082cf8
rahul-arakka <arakkarahul11@gmail.com>
2022-02-15 17:03:00 +0530

    Project details Done

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("We are in Users Page");
+
+let projectDetails = document.getElementById("projectDetails");
+let personalDetails = document.getElementById("personalDetails");
+
+projectDetails.addEventListener("click", getProjects);
+
+function getProjects() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "project.JSON", true);
+
+  xhr.onload = function () {
+    let json = JSON.parse(this.responseText);
+    let art = json.project;
+    console.log(art);
+    let html = `<thead>
+                    <tr>
+                    <th scope="col">S No.</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    </tr>
+                </thead>`;
+    art.forEach(function (element, index) {
+      html += `<tr>
+                <td>${index + 1}</td>
+                <td>${element.id}</td>
+                <td>${element.location}</td>
+              </tr>`;
+    });
+    let table = document.getElementById("table");
+    table.innerHTML = html;
+  };
+  xhr.send();
+  console.log("get request");
+}
