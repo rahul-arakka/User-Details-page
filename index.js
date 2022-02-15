ac4dab9c14796d27342965bc4e36cb3235d58baa
rahul-arakka <arakkarahul11@gmail.com>
2022-02-15 17:27:26 +0530

    json changed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ projectDetails.addEventListener("click", getProjects);
 function getProjects() {
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "project.JSON", true);
+  xhr.open("GET", '/project.json', true);
 
   xhr.onload = function () {
     let json = JSON.parse(this.responseText);
